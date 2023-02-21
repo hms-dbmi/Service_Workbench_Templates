acef22b20cd69e1711bad2dcd75f8c6a532d91a2
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2023-02-21 10:24:46 -0500

    Update provisioner to run additional r packages.

--- RStudio/machine-images/config/infra/provisioners/provision-rstudio.sh
+++ RStudio/machine-images/config/infra/provisioners/provision-rstudio.sh
@@ -4,7 +4,7 @@
 sudo yum install -y gcc-7.3.* gcc-gfortran-7.3.* gcc-c++-7.3.*
 sudo yum install -y java-1.8.0-openjdk-devel-1.8.0.*
 sudo yum install -y readline-devel-6.2 zlib-devel-1.2.* bzip2-devel-1.0.* xz-devel-5.2.* pcre-devel-8.32
-sudo yum install -y libcurl-devel-7.79.1 libpng-devel-1.5.* cairo-devel-1.15.* pango-devel-1.42.*
+sudo yum install -y libcurl-devel-7.87.0 libpng-devel-1.5.* cairo-devel-1.15.* pango-devel-1.42.*
 sudo yum install -y xorg-x11-server-devel-1.20.* libX11-devel-1.6.* libXt-devel-1.1.*
 
 # Install R from source (https://docs.rstudio.com/resources/install-r-source/)
@@ -18,6 +18,9 @@ cd "R-${R_VERSION}/"
 sudo make
 sudo make install
 cd "../../.."
+
+# Cleanup R install tmp folder
+sudo rm -rf "/tmp/R"
 
 # Install RStudio
 rstudio_rpm="rstudio-server-rhel-1.4.1717-x86_64.rpm"
@@ -74,6 +77,8 @@ sudo crontab -l 2>/dev/null > "/tmp/crontab"
 echo '*/2 * * * * /usr/local/bin/check-idle 2>&1 >> /var/log/check-idle.log' >> "/tmp/crontab"
 sudo crontab "/tmp/crontab"
 
+# Cleanup RStudio install tmp folder
+sudo rm -rf "/tmp/rstudio"
 
 # Install system packages necessary for installing R packages through RStudio CRAN [devtools, tidyverse]
 sudo yum install -y git-2.23.* openssl-devel-1.0.* libxml2-devel-2.9.*
@@ -85,6 +90,8 @@ sudo yum install -y "/tmp/libgit2/${libgit2_rpm}"
 curl -s "http://mirror.centos.org/centos/7/extras/x86_64/Packages/${libgit2_devel_rpm}" > "/tmp/libgit2/${libgit2_devel_rpm}"
 sudo yum install -y "/tmp/libgit2/${libgit2_devel_rpm}"
 
+# Cleanup libgit2 install tmp folder
+sudo rm -rf "/tmp/libgit2"
 
 # Other recommended system packages for installing R packages (https://docs.rstudio.com/rsc/post-setup-tool/)
 sudo yum groupinstall -y 'Development Tools'            # Compiling tools 
@@ -103,18 +110,3 @@ sudo yum install freetype-devel-2.8 harfbuzz-devel-1.7.*          # Font
 sudo yum install -y mariadb-devel-5.5.*                       # MariaDB/MySQL client & server packages
 sudo yum install -y unixODBC-devel-2.3.*                      # ODBC API client
 sudo yum install -y gmp-devel-6.0.*                           # GNU MP arbitrary precision library
-
-#Additional R Packages
-sudo su - -c "R -e \"install.packages('tidyverse', version='1.3.1', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('devtools', version='2.4.0', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('kableExtra', version='1.3.4', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('survival', version='3.2.10', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('survminer', version='0.4.9', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('MASS', version='7.3.53.1', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('quantreg', version='5.85', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('DescTools', version='0.99.41', repos='http://cran.rstudio.com/')\""
-
-# Wipe out all traces of provisioning files
-sudo rm -rf "/tmp/rstudio"
-sudo rm -rf "/tmp/libgit2"
-sudo rm -rf "/tmp/R"
