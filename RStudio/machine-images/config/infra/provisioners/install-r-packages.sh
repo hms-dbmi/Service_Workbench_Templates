bfa3dc48e99899c1fc822932b0017629cb1b7b15
bp85 <bhanu_prasad@hms.harvard.edu>
2023-07-07 08:55:15 -0400

    working improvements

--- RStudio/machine-images/config/infra/provisioners/install-r-packages.sh
+++ RStudio/machine-images/config/infra/provisioners/install-r-packages.sh
@@ -21,14 +21,22 @@ echo 'options(repos=c(CRAN="https://cran.rstudio.com"))' | sudo tee -a /root/.Rp
 echo 'options(Ncpus=parallel::detectCores() * 2)' | sudo tee -a /root/.Rprofile
 
 sudo cat << END >> "$tmpFile"
-install.packages('devtools')
+# Install and load remotes package
+install.packages('remotes', version='2.4.2')
+library('remotes')
+
+# Install repos that need a specific order
+remotes::install_github('rstudio/httpuv', version='1.6.9', dep=TRUE)
+install.packages('shiny', version='1.7.4') # needs httpuv
+install.packages('devtools', version='2.4.5') # needs shiny
+install.packages('adegenet', version='2.1.10') # needs devtools
 
 packages <- c(
 'tidyverse=1.3.2',
 'dplyr=1.1.0',
 'tidyr=1.3.0',
 'ggplot2=3.4.1',
-'data.table=1.14.6,
+'data.table=1.14.6',
 'kableExtra=1.3.4',
 'survival=3.5.3',
 'survminer=0.4.9',
@@ -46,7 +54,7 @@ packages <- c(
 'maptools=1.1.6',
 'randomForest=4.7.1.1',
 'RCurl=1.98.1.10',
-'arsenal=3.6.3',
+'arsenal=3.6.3'
 )
 
 for (pkg in packages) {
@@ -56,21 +64,12 @@ for (pkg in packages) {
   devtools::install_version(pkg_name, version = pkg_version, dependencies = TRUE)
 }
 
-# Install and load remotes package
-install.packages('remotes', version='2.4.2')
-library('remotes')
-
 remotes::install_github('YuLab-SMU/ggtree', version='3.7.1.2', dep=TRUE)
 
-# Install repos that need a specific order
-remotes::install_github('rstudio/httpuv', version='1.6.9', dep=TRUE)
-install.packages('shiny', version='1.7.4') # needs httpuv
-install.packages('devtools', version='2.4.5') # needs shiny
-install.packages('adegenet', version='2.1.10') # needs devtools
 END
 
 # Execute installation script
-sudo su - -c "R -e \"source('$tmpFile')\""
+sudo su - -c "export TMPDIR=$tmpDir; Rscript $tmpFile"
 
 # Output R library packages
 sudo su - -c "R -e \"installed.packages(lib.loc='/usr/local/lib64/R/library')[,'Version']\""
--- RStudio/machine-images/config/infra/provisioners/provision-rstudio.sh
+++ RStudio/machine-images/config/infra/provisioners/provision-rstudio.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
-sudo timedatectl set-timezone UTC
+# ADD IPtables rules
+sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+sudo service iptables save
+
+sudo yum-config-manager --setopt="epel.priority=10" epel --save || true
+
+#SELinux stuff
+sudo /sbin/selinuxenabled >& /dev/null
+if [ $? -eq 0 ]; then
+  export SELINUX=true
+  sudo yum install -y policycoreutils-python setroubleshoot setools
+  sudo setsebool -P httpd_can_network_relay 1
+  sudo semanage port -m -t http_port_t -p tcp 8787
+  export TMP_DIR="/home/ec2-user"
+fi
+
+sudo timedatectl status
+
+[ -z "$SELINUX" ] && sudo timedatectl set-timezone UTC
 
 # Various development packages needed to compile R
+sudo yum downgrade -y libcurl curl
 sudo yum install -y gcc-7.3.* gcc-gfortran-7.3.* gcc-c++-7.3.*
 sudo yum install -y java-1.8.0-openjdk-devel-1.8.0.*
 sudo yum install -y readline-devel-6.2 zlib-devel-1.2.* bzip2-devel-1.0.* xz-devel-5.2.* pcre-devel-8.32
@@ -11,9 +30,9 @@ sudo yum install -y xorg-x11-server-devel-1.20.* libX11-devel-1.6.* libXt-devel-
 
 # Install R from source (https://docs.rstudio.com/resources/install-r-source/)
 R_VERSION="4.1.3"
-mkdir -p "/tmp/R/"
-curl -s "https://cran.r-project.org/src/base/R-4/R-${R_VERSION}.tar.gz" > "/tmp/R/R-${R_VERSION}.tar.gz"
-cd "/tmp/R/"
+mkdir -p "$TMP_DIR/tmp/R/"
+cd "$TMP_DIR/tmp/R/"
+curl -s "https://cran.r-project.org/src/base/R-4/R-${R_VERSION}.tar.gz" > "R-${R_VERSION}.tar.gz"
 tar xvf "R-${R_VERSION}.tar.gz"
 cd "R-${R_VERSION}/"
 ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-pcre1
@@ -22,30 +41,13 @@ sudo make install
 cd "../../.."
 
 # Cleanup R install tmp folder
-sudo rm -rf "/tmp/R"
-
-# ADD IPtables rules
-sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
-sudo service iptables save
-
-#SELinux stuff
-sudo /sbin/selinuxenabled >& /dev/null
-if [ $? -eq 0 ]; then
-  sudo yum install -y policycoreutils-python setroubleshoot setools
-  sudo chcon -R -t bin_t /usr/lib/rstudio-server/bin/
-  sudo setsebool -P httpd_can_network_relay 1
-  sudo semanage port -m -t http_port_t -p tcp 8787
-  export TMP_DIR="/home/ec2-user/tmp"
-else
-  export TMP_DIR="/tmp"
-fi
+sudo rm -rf "$TMP_DIR/tmp/R"
 
 # Install RStudio
 rstudio_rpm="rstudio-server-rhel-1.4.1717-x86_64.rpm"
-curl -s "https://download2.rstudio.org/server/centos7/x86_64/${rstudio_rpm}" > "/tmp/rstudio/${rstudio_rpm}"
-sudo yum install -y "/tmp/rstudio/${rstudio_rpm}"
-sudo systemctl enable rstudio-server
-sudo systemctl restart rstudio-server
+sudo yum install -y "https://download2.rstudio.org/server/centos7/x86_64/${rstudio_rpm}"
+[ -n "$SELINUX" ] && sudo chcon -R -t bin_t /usr/lib/rstudio-server/bin/
+sudo systemctl enable --now rstudio-server
 
 # Create a user for RStudio to use; its password is set at boot time
 sudo useradd -m rstudio-user
@@ -53,9 +55,9 @@ sudo useradd -m rstudio-user
 #Generate self signed certificate
 commonname=$(uname -n)
 password=dummypassword
-mkdir -p "/tmp/rstudiov2/ssl"
-chmod 700 /tmp/rstudiov2/ssl
-cd /tmp/rstudiov2/ssl
+mkdir -p "/tmp/rstudiov2/ssl" "$TMP_DIR/tmp/rstudiov2/ssl"
+chmod 700 $TMP_DIR/tmp/rstudiov2/ssl
+cd $TMP_DIR/tmp/rstudiov2/ssl
 openssl genrsa -des3 -passout pass:$password -out cert.key 2048
 #Remove passphrase from the key. Comment the line out to keep the passphrase
 openssl rsa -in cert.key -passin pass:$password -out cert.key
@@ -67,8 +69,8 @@ cd "../../.."
 # Install and configure nginx
 sudo amazon-linux-extras install -y nginx1
 sudo openssl dhparam -out "/etc/nginx/dhparam.pem" 2048
-sudo mv "/tmp/rstudiov2/ssl/cert.pem" "/etc/nginx/"
-sudo mv "/tmp/rstudiov2/ssl/cert.key" "/etc/nginx/"
+sudo mv "$TMP_DIR/tmp/rstudiov2/ssl/cert.pem" "/etc/nginx/"
+sudo mv "$TMP_DIR/tmp/rstudiov2/ssl/cert.key" "/etc/nginx/"
 sudo mv "/tmp/rstudio/nginx.conf" "/etc/nginx/"
 sudo chown -R nginx:nginx "/etc/nginx"
 sudo chmod -R 600 "/etc/nginx"
@@ -81,7 +83,6 @@ sudo chmod 600 "/root/secret.txt"
 sudo mv "/tmp/rstudio/set-password" "/usr/local/bin/"
 sudo chown root: "/usr/local/bin/set-password"
 sudo chmod 775 "/usr/local/bin/set-password"
-sudo sh "/usr/local/bin/set-password"
 echo '@reboot /usr/local/bin/set-password 2>&1 >> /var/log/set-password.log' | sudo tee -a /var/spool/cron/root
 
 # Install script that checks idle time and shuts down if max idle is reached
@@ -91,20 +92,14 @@ sudo chmod 775 "/usr/local/bin/check-idle"
 echo '*/2 * * * * /usr/local/bin/check-idle 2>&1 >> /var/log/check-idle.log' | sudo tee -a /var/spool/cron/root
 
 # Cleanup RStudio install tmp folder
-sudo rm -rf "/tmp/rstudio"
+sudo rm -rf "/tmp/rstudio" "$TMP_DIR/tmp/rstudiov2/ssl"
 
 # Install system packages necessary for installing R packages through RStudio CRAN [devtools, tidyverse]
 sudo yum install -y git-2.23.* openssl-devel-1.0.* libxml2-devel-2.9.*
 libgit2_rpm="libgit2-0.26.6-1.el7.x86_64.rpm"
 libgit2_devel_rpm="libgit2-devel-0.26.6-1.el7.x86_64.rpm"
-mkdir -p "/tmp/libgit2/"
-curl -s "http://mirror.centos.org/centos/7/extras/x86_64/Packages/${libgit2_rpm}" > "/tmp/libgit2/${libgit2_rpm}"
-sudo yum install -y "/tmp/libgit2/${libgit2_rpm}"
-curl -s "http://mirror.centos.org/centos/7/extras/x86_64/Packages/${libgit2_devel_rpm}" > "/tmp/libgit2/${libgit2_devel_rpm}"
-sudo yum install -y "/tmp/libgit2/${libgit2_devel_rpm}"
-
-# Cleanup libgit2 install tmp folder
-sudo rm -rf "/tmp/libgit2"
+sudo yum install -y "http://mirror.centos.org/centos/7/extras/x86_64/Packages/${libgit2_rpm}"
+sudo yum install -y "http://mirror.centos.org/centos/7/extras/x86_64/Packages/${libgit2_devel_rpm}"
 
 # Other recommended system packages for installing R packages (https://docs.rstudio.com/rsc/post-setup-tool/)
 sudo yum groupinstall -y 'Development Tools'            # Compiling tools
