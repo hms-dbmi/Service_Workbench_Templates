e9ecae464f232a70eb7867a457d2081c66679e4f
Luke Sikina <lucas.skina@gmail.com>
2024-08-21 14:25:22 -0400

    [ALS-6896] - Fix provisioning scripts

--- RStudio/machine-images/config/infra/provisioners/provision-rstudio.sh
+++ RStudio/machine-images/config/infra/provisioners/provision-rstudio.sh
@@ -6,7 +6,9 @@ sudo timedatectl set-timezone UTC
 sudo yum install -y gcc-7.3.* gcc-gfortran-7.3.* gcc-c++-7.3.*
 sudo yum install -y java-1.8.0-openjdk-devel-1.8.0.*
 sudo yum install -y readline-devel-6.2 zlib-devel-1.2.* bzip2-devel-1.0.* xz-devel-5.2.* pcre-devel-8.32
-sudo yum install -y libcurl-devel-7.88.* libpng-devel-1.5.* cairo-devel-1.15.* pango-devel-1.42.*
+sudo yum downgrade -y curl-7.88.1-1.amzn2.0.1 libcurl-7.88.1-1.amzn2.0.1
+sudo yum install -y libcurl-devel-7.88.*
+sudo yum install -y libpng-devel-1.5.* cairo-devel-1.15.* pango-devel-1.42.*
 sudo yum install -y xorg-x11-server-devel-1.20.* libX11-devel-1.6.* libXt-devel-1.1.*
 
 # Install R from source (https://docs.rstudio.com/resources/install-r-source/)
